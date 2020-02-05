5640893cdf68e97dfd37363e2e987f136d4192e9
Nikolay Geldyev <nikoejento@gmail.com>
2020-02-05 14:21:55 +0300

    add header

--- src/views/Clips.vue
+++ src/views/Clips.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
+    <v-parallax height="300" :src="headerImage" style="background: #202020">
+      <v-layout fill-height align-center column justify-center>
+        <router-link
+          tag="h1"
+          :to="`/${$route.params.streamer}`"
+          class="display-3 font-weight-thin mb-1 text-uppercase pointer link"
+        >
+          {{ $route.params.streamer }}
+        </router-link>
+        <router-link tag="h4" to="/" class="title font-weight-bold text-uppercase pointer link">
+          StreamArchive
+        </router-link>
+      </v-layout>
+    </v-parallax>
     <v-container fluid grid-list-md>
       <v-layout wrap>
         <v-flex xs12 md9 class="mb-4">
@@ -14,7 +28,7 @@
             <v-flex xs8 md12 class="title">{{ stream.title }}</v-flex>
             <v-flex xs4 md12 class="subtitle-2 font-italic text-xs-right text-md-left">{{ stream.game }}</v-flex>
             <v-flex xs12 md12 class="subtitle-1">{{ dateString }}</v-flex>
-            <v-flex xs12 md12 class="subtitle-1">{{ getVideoTime }}</v-flex>
+            <v-flex xs12 md12 class="subtitle-1"></v-flex>
           </v-layout>
         </v-flex>
       </v-layout>
